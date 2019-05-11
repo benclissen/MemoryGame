6932711bceea77573e121fbc9d77cf3e0c2f9e02
Ben Clissen <benclissen@me.com>
2019-05-11 13:28:58 +0100

    Halfway done

--- js/app.js
+++ js/app.js
@@ -10,6 +10,170 @@
  *   - add each card's HTML to the page
  */
 
+
+//variables
+const deck = document.querySelector(".deck");
+deck.addEventListener('click', openCard);
+const movesCount = document.querySelector('.moves');
+let cardSymbols = ['diamond', 'paper-plane-o', 'anchor', 'bolt', 'cube', 'leaf', 'bicycle', 'bomb', 'diamond', 'paper-plane-o', 'anchor', 'bolt', 'cube', 'leaf', 'bicycle', 'bomb'];
+let cards = [].slice.call(deck.children);
+let openedCards = [];
+
+let timer = 0;
+let elapsedSec = 0;
+let gameInProgress = false;
+let moves = 0
+let matches = 8;
+
+//run
+newGame();
+
+//Initialise
+
+function newGame() {
+
+    // Clear openedCards array
+    openedCards = [];
+
+    // Shuffle symbols
+    cardSymbols = shuffle(cardSymbols);
+
+    // Iterate over all cards
+    cards.forEach((card, index) => {
+        // Remove classes
+        card.classList.remove('open', 'show', 'match', 'bounceIn');
+        // Remove symbols
+        removeClassByPrefix(card.children[0], 'fa-');  //need to rework this
+
+        // Attach new symbols to cards
+        const symbol = `fa-${cardSymbols[index]}`;
+        card.children[0].classList.add(symbol);
+    });
+	elapsedSec = 0;
+//	openAllCards();
+	
+	// Reset moves
+    moves = 0;
+    movesCount.textContent = moves;
+}
+
+function openCard(event) {
+
+    startTimer();
+
+    var target = event.target;
+    const parent = target.parentElement;
+    if (parent.classList.contains('card')) {
+        target = parent;
+    }
+
+    if (!openedCards.includes(target)) {
+        target.classList.add('open', 'show');
+        openedCards.push(target);
+        checkMatch();
+    }
+}
+
+function checkMatch() {
+    const length = openedCards.length;
+    if (length === 2) {
+
+        const last = openedCards[1];
+        const preLast = openedCards[0];
+
+		if (last.children[0].classList.toString() === //Cards Match
+            preLast.children[0].classList.toString()) {
+            matches++;
+			cardsMatched(preLast);
+            cardsMatched(last);
+        } else { // Card Not Matching
+            cardsNotMatched(preLast);
+			cardsNotMatched(last);
+        }
+        incrementMove();
+        openedCards = [];
+        checkIfWon();
+    }
+}
+
+
+
+function openAllCards() {
+	cards.forEach((card, index) => {
+        card.classList.add('show');
+		console.log('card openend' + index);
+	});
+}
+
+function incrementMove() {
+    moves++;
+    movesCount.textContent = moves;
+    //determineRating();
+}
+
+function cardsMatched(card) {
+    setTimeout(() => {
+        card.classList.add('match', 'bounceIn');
+    }, 500)
+}
+function cardsNotMatched(card) {
+    setTimeout(() => {
+        card.classList.remove('open', 'show');
+    }, 500)
+}
+
+function checkIfWon() {
+    if (matches === 8) {
+        stopTimer();
+        openModal();
+    }
+}
+
+//we're not using this function
+function resetScore() {
+
+    // Reset rating
+//    rating = 3;
+//    stars.forEach(star => removeClassByPrefix(star, 'empty-star'));
+
+
+
+    // Reset matches
+    matches = 0;
+
+    // Reset time
+    elapsedSec = 0;
+//    timerHours.textContent = '00';
+//    timerMins.textContent = '00';
+//    timerSeconds.textContent = '00';
+
+    // Stop timer
+    stopTimer();
+}
+
+function startTimer() {
+    if (!gameInProgress) {
+        gameInProgress = true;
+        timer = setInterval(setTime, 1000);
+    }
+}
+
+function stopTimer() {
+    gameInProgress = false;
+    clearInterval(timer);
+}
+
+function setTime() {
+    let remainingSec = ++elapsedSec;
+//    hour = parseInt(remainderSeconds / 3600);
+//    timerHours.textContent = stringifyTime(hour);
+//    remainderSeconds = remainderSeconds % 3600;
+//    min = parseInt(remainderSeconds / 60)
+//    timerMins.textContent = stringifyTime(min);
+//    remainderSeconds = remaind
+    //timerSeconds.textContent = remainingSec;
+}
+
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -21,8 +185,14 @@ function shuffle(array) {
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
     }
-
     return array;
+
+}
+
+function removeClassByPrefix(el, prefix, replace = '') {
+    var regx = new RegExp('\\b' + prefix + '(.*)?\\b', 'g');
+    el.className = el.className.replace(regx, replace);
+    return el;
 }
 
 
